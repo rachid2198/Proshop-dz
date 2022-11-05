d3567d3649aa15f1a5de0f6c0a74863038fd5682
rachid <you@example.com>
2022-11-06 00:53:36 +0100

    last commit

--- base/views/order_views.py
+++ base/views/order_views.py
@@ -11,6 +11,21 @@ from base.serializers import ProductSerializer,OrderSerializer
 from django.contrib.auth.hashers import make_password
 from rest_framework import status
 
+from django.views import View
+from django.http import HttpResponse, HttpResponseNotFound
+import os
+
+class Assets(View):
+
+    def get(self, _request, filename):
+        path = os.path.join(os.path.dirname(__file__), 'static', filename)
+
+        if os.path.isfile(path):
+            with open(path, 'rb') as file:
+                return HttpResponse(file.read(), content_type='application/javascript')
+        else:
+            return HttpResponseNotFound()
+
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def addOrderItems(request):
