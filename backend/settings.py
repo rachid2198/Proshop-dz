099d7c42de14173733436aae4c2608b86f0fabe5
rachid <you@example.com>
2022-11-06 02:51:27 +0100

    last commit

--- backend/settings.py
+++ backend/settings.py
@@ -16,8 +16,10 @@ from datetime import timedelta
 import django_heroku
 import dj_database_url
 import os
+import mimetypes
 
-
+mimetypes.add_type("text/css", ".css", True)
+mimetypes.add_type("text/javascript", ".js", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
