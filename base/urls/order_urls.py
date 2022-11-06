c666bd7b6626836a72b1bd64ef41949191e054d7
rachid <you@example.com>
2022-11-06 01:58:47 +0100

    last commit

--- base/urls/order_urls.py
+++ base/urls/order_urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path
-from base.views import order_views as views
+from ..views import order_views as views
 
 
 urlpatterns = [
--- base/urls/user_urls.py
+++ base/urls/user_urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path
-from base.views import user_views as views
+from ..views import user_views as views
 
 
 urlpatterns = [
