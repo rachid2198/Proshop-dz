18959c50c265dd46bd2681be0ab3c7fe7b58e3e3
rachid <you@example.com>
2022-11-07 17:08:59 +0100

    last commit

--- base/views/product_views.py
+++ base/views/product_views.py
@@ -22,7 +22,7 @@ def getProducts(request):
     if query==None:
         query=""
     
-    products=Product.objects.filter(name__icontains=query)
+    products=Product.objects.filter(name__icontains=query).order_by('-numReviews')
     serializer=ProductSerializer(products, many=True)
     return Response(serializer.data)
 
