c89478d7d0f792e3a409bcf3968f5abcfcba42f5
rachid2198 <rachid9821@gmail.com>
2022-12-10 23:08:27 +0100

    last commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import './bootstrap.min.css';
 import App from './App';
@@ -9,13 +9,14 @@ import './interceptors/axios'
 import {store} from './store'
 import { Provider } from 'react-redux';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+ReactDOM.render(
   <>
     <Provider store={store}>
       <App />
     </Provider>
-  </>
+  </>,
+  document.getElementById('root') 
 );
 
 // If you want to start measuring performance in your app, pass a function
