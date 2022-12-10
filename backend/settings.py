5381dbaa5b04882120c28f77cab93809a90a538b
rachid2198 <rachid9821@gmail.com>
2022-12-10 20:48:18 +0100

    last commit

--- backend/settings.py
+++ backend/settings.py
@@ -127,26 +127,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+
 DATABASES = {
-        'default': {
-            'ENGINE': 'djongo',
-            'NAME': 'proshop',
-            'ENFORCE_SCHEMA': False,
-            'CLIENT': {
-                'host': 'mongodb+srv://rachid:znq@coding-addict-tut.pamhaiy.mongodb.net/?retryWrites=true&w=majority'
-            }  
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-
-# DATABASES = {}
-# DATABASES['default'] = dj_database_url.config(conn_max_age=600)
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 
 # Password validation
