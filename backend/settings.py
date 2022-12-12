5f89a477971725ee0e5a81474940aa2e12a89a65
rachid2198 <rachid9821@gmail.com>
2022-12-12 01:20:46 +0100

    first commit

--- backend/settings.py
+++ backend/settings.py
@@ -178,9 +178,9 @@ CLOUDINARY_STORAGE = {
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL='/media/'
