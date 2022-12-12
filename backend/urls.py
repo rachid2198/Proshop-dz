dec2a47de1d8089f9c9c3818a3fa8856999462cb
rachid2198 <rachid9821@gmail.com>
2022-12-12 02:01:06 +0100

    first commit

--- backend/urls.py
+++ backend/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path('api/products/',include('base.urls.product_urls')),
     path('api/users/',include('base.urls.user_urls')),
     path('api/orders/',include('base.urls.order_urls')),
-    #re_path('',TemplateView.as_view(template_name='index.html'))
+    re_path('',TemplateView.as_view(template_name='index.html'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
